66195d52a7832d9ddbae46cd047792069876671c
Jenny Chu <jchudesign0930@gmail.com>
2019-02-24 23:45:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require "pry"
+katz_deli = []
+
+def line (katz_deli)
+ katz_deli = 0
+while katz_deli == 0
+  puts "The line is currently empty."
+  
+end
+end
+  
+def take_a_numnber (katz_deli, name) 
+
+  katz_deli = [1,2,3]
+  puts "Welcom,#{name}. You are number #{sits} in line "
+ sits += 1
+end
+
+
+
+def now_serving (array_line)
+  if array_assigned.length > 0
+    puts "Currently serving #{array_line[0]}."
+    array_assigned.shift
+  else 
+    puts "There is nobody waiting to be served!"
+  end
+end
